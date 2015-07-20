b47ed0e5f34041c4207a2b14f9a927a04999ceac
Javi <caleop@yahoo.es>
2015-07-21 00:37:39 +0200

    commit 2

--- src/Temperaturas.java
+++ src/Temperaturas.java
@@ -1,4 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 
 public class Temperaturas {
-
+	public static void main(String[] args) throws IOException{
+		ArrayList temperaturas = new ArrayList();
+		String opcion;
+		BufferedReader bf = new
+				BufferedReader(new InputStreamReader(System.in));
+		do {
+			
+		}
+	}
 }
